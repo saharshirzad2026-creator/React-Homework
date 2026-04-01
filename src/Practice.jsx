532a0d1374f37877169f5f8df17a407aa82d3eff
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-03-31 19:01:54 -0700

    a few changes

---
+++ src/Practice.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function Practice(){
+    const [value, setValue] = useState(0);
+    return (
+        <div className="w-full flex justify-between px-12 my-40">
+            <button onClick={()=> setValue(value+1)} className="border-2 px-4 py-4 bg-green-400 rounded-md border-green-600 font-bold">Increment</button>
+            <h1 className="border rounded-full bg-green-400 border-green-600 px-6 py-4 font-bold text-xl">{value}</h1>
+            <button onClick={()=> setValue(value-1)} className="border-2 px-4 py-4 bg-green-400 rounded-md border-green-600 font-bold">Decrement</button>
+        </div>
+    )
+}
+export default Practice;
