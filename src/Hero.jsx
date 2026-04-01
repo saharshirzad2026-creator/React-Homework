6b0111708bf1e9e0c8963ffe849bf7c26514e950
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-03-31 18:58:16 -0700

    a few changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,11 @@
-function Hero(name){
-    return <div>Hello my name is {name}</div>
+import Practice from "./Practice";
+
+function Hero(props){
+    return(
+    <>
+       <h1 className="text-red-600 text-center my-8 text-3xl font-bold">{props.text}</h1>
+       <Practice/>
+    </>
+    );
 }
 export default Hero;
