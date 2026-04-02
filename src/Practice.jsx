003e571605c80f85b80fed1d93dd6e9d8c876d45
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-01 18:52:24 -0700

    changes

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import SecondPractice from "./SecondPractice";
 
 function Practice(){
     const [value, setValue] = useState(0);
@@ -7,6 +8,7 @@ function Practice(){
             <button onClick={()=> setValue(value+1)} className="border-2 px-4 py-4 bg-green-400 rounded-md border-green-600 font-bold">Increment</button>
             <h1 className="border rounded-full bg-green-400 border-green-600 px-6 py-4 font-bold text-xl">{value}</h1>
             <button onClick={()=> setValue(value-1)} className="border-2 px-4 py-4 bg-green-400 rounded-md border-green-600 font-bold">Decrement</button>
+            <SecondPractice/>
         </div>
     )
 }
