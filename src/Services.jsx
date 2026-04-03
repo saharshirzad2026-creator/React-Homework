5b4c4fa5fb694f19702112f97b1d06e28020221d
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-02 19:04:37 -0700

    some changes

--- src/Services.jsx
+++ src/Services.jsx
@@ -4,7 +4,7 @@ function Services(){
     return (
         <div>
             <Navbar/>
-        <h1 className="text-3xl text-center font-bold text-fuchsia-700 my-3">This is the Services page</h1>
+        <h1 className="text-3xl text-center font-bold border-2 border-b-rose-950 mx-auto p-5 text-fuchsia-700 my-3">This is the Services page</h1>
         </div>
     )
 }
