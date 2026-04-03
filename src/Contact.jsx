072add52d8c45f3659d38a80c963c32224127d4b
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-02 19:00:14 -0700

    some changes

---
+++ src/Contact.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+function Contact(){
+    return (
+        <div>
+            <Navbar/>
+        <h1 className="text-3xl text-center font-bold bg-linear-60 from-green-600 to-green-800 text-transparent bg-clip-text my-20">This is the Contact page</h1>
+        </div>
+    )
+}
+export default Contact;
