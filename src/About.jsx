ad47fe19d0a83bd8b8d21d1dee33ec7fd03359f2
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-02 18:59:13 -0700

    some changes

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+function About(){
+    return (
+        <div>
+            <Navbar/>
+        <h1 className="text-center text-3xl font-bold text-blue-700 my-20 border-2 p-5 w-6xl mx-auto">Hello guys This is the About page</h1>
+        </div>
+    )
+}
+export default About;
