b3c8c262088190b712b6548cfb4e46e173e2c51c
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-02 19:02:18 -0700

    some changes

---
+++ src/Services.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+function Services(){
+    return (
+        <div>
+            <Navbar/>
+        <h1 className="text-3xl text-center font-bold text-fuchsia-700 my-3">This is the Services page</h1>
+        </div>
+    )
+}
+export default Services;
