ee86a38170619704b053bfdaf93ac86bf6bde560
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-02 18:56:42 -0700

    some new files

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Services from './Services.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/services' element={<Services/>}/>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
