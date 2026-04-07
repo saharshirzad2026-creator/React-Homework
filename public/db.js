5d313f62015944083f59461310ade79713a1f09c
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-07 09:35:41 -0700

    some changes

---
+++ public/db.js
@@ -0,0 +1,44 @@
+export const studentsList = [
+        {
+            id: 1,
+            name: "Zahra",
+            score: 50,
+            grade: 7,
+            details: "She is one of our intellegent student",
+        },
+        {
+            id: 2,
+            name: "Rahim",
+            score: 100,
+            grade: 10,
+            details: "He is one of our intellegent student",
+        },
+        {
+            id: 3,
+            name: "Hamid",
+            score: 89,
+            grade: 9,
+            details: "He is one of our intellegent student",
+        },
+    ];
+
+export const schoolsList = [
+    {
+        id: 1,
+        name: "Austad Sharifi",
+        manager: "Esmatullah Ghulami",
+        details: "This is a big school for male students in Hotqul."
+    },
+    {
+        id: 2,
+        name: "Bibi Zahra",
+        manager: "Zohra Qurbani",
+        details: "This is a big school for female students witch teach students from grade one to six."
+    },
+    {
+        id: 3,
+        name: "Hotqul Angori",
+        manager: "Ali Qasimi",
+        details: "This is a big school for male students in Angori."
+    },
+]
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './About.jsx'
 import Contact from './Contact.jsx'
 import Services from './Services.jsx'
+import Student from './Student.jsx'
+import Details from './Details.jsx'
+import School from './School.jsx'
+import SchoolDetails from './schoolDetails.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +19,10 @@ createRoot(document.getElementById('root')).render(
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/services' element={<Services/>}/>
+          <Route path='student' element={<Student/>}/>
+          <Route path='student/:id' element={<Details/>}/>
+          <Route path='school' element={<School/>}/>
+          <Route path='school/:name' element={<SchoolDetails/>}/>
         </Routes>
     </BrowserRouter>
   </StrictMode>,
