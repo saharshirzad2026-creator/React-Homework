d68b13e7b959adde00a17fda1c5f7ae62b05ebf6
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-08 10:22:05 -0700

    some changes

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Services from './Services.jsx'
+import Student from './Student.jsx'
+import Details from './Details.jsx'
+import School from './School.jsx'
+import SchoolDetails from './schoolDetails.jsx'
+import Teacher from './Teachers.jsx'
+import { Detailspage } from './Detailspage.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='student' element={<Student/>}/>
+          <Route path='student/:id' element={<Details/>}/>
+          <Route path='school' element={<School/>}/>
+          <Route path='school/:name' element={<SchoolDetails/>}/>
+          <Route path='teachers' element={<Teacher/>}/>
+          <Route path='details/:name' element={<Detailspage/>}/>
+        </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
