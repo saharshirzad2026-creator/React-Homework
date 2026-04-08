788e04dd1ee2d942f2665e3fcc205e934eca5898
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-08 10:22:51 -0700

    some changes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { Connect } from "vite";
+// import { Route, Routes } from "react-router-dom";
+// import About from "./About";
+// import Hero from "./Hero";
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+// import Services from "./Services";
+// import Contact from "./Contact";
+
+const App = ()=>{
+  const navigate = useNavigate();
+  function handleClick(){
+    navigate('/teachers');
+  }
+  return(
+    <>
+      <Navbar/>
+      <h1 className="text-3xl font-bold text-center text-red-700 my-3">Hello and good afternoon everyone This is the home page</h1>
+       {/* <Hero text="Hello and good afternoon everyone"/> */}
+       <button className="bg-red-700 text-2xl font-bold mx-96 p-3 rounded-md text-white my-5" onClick={handleClick}>Go to Teachers page</button>
+    </>
+  )
+}
+export default App;
