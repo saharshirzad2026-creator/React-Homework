9f3e079de497581564f2c7059bb9ddede5c61f1e
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-09 19:11:53 -0700

    some changes

--- src/Test.jsx
+++ src/Test.jsx
@@ -16,3 +16,24 @@ export default function Testing(){
                     <h1 className="text-5xl text-red-600 font-bold">No user exists</h1>
                 </div>
             )}
+            {list.length > 0 &&(
+                <div className="max-w-6xl mx-auto">
+                    <table className="border w-full">
+                        <tr>
+                            <th className="border py-1 px-3 text-center">Name</th>
+                            <th className="border py-1 px-3 text-center">Email</th>
+                            <th className="border py-1 px-3 text-center">Phone Number</th>
+                        </tr>
+                        {list.map(user=>(
+                            <tr>
+                                <td className="border py-1 px-3 text-center">{user.name}</td>
+                                <td className="border py-1 px-3 text-center">{user.email}</td>
+                                <td className="border py-1 px-3 text-center">{user.phone}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            )}
+        </div>
+    )
+}
